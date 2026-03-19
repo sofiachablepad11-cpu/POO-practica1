d7440e0aac11a05480757088a5f812f40d8069f1
sofiachablepad11-cpu <sofiachablepad.11@gmail.com>
2026-03-18 23:38:03 -0600

    PRIMER COMMIT

---
+++ BIENVENIDASYSTEM.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BIENVENIDASYSTEM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("¿Comó te llamas?");
+		Scanner sc= new Scanner(System.in);
+		String respuesta = sc.next();
+		System.out.println("Hola " +respuesta+" Bienvenido (a) a la programación");
+		sc.close();
+
+	}
+
+}
